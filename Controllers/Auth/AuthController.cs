47f9549c943d44136b0810deff08f9162ed71d9f
Ravi-Webdevloper <ravichaurasiya392@gmail.com>
2024-10-01 23:32:01 +0530

    add authentication controllrer

---
+++ Controllers/Auth/AuthController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Projects.Controllers.Auth
+{
+    public class AuthController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
